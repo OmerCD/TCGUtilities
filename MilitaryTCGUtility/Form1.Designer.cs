be440e2a6afb4208262d1143ecc512120304cebc
Ömer Can Danacıoğlu <omercd@hotmail.com.tr>
2018-12-15 22:27:02 +0300

    Add project files.

---
+++ MilitaryTCGUtility/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace MilitaryTCGUtility
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tBName = new System.Windows.Forms.TextBox();
+            this.tBDescription = new System.Windows.Forms.TextBox();
+            this.lBKeywords = new System.Windows.Forms.ListBox();
+            this.bAdd = new System.Windows.Forms.Button();
+            this.bEdit = new System.Windows.Forms.Button();
+            this.bDelete = new System.Windows.Forms.Button();
+            this.tBSearch = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tBName
+            // 
+            this.tBName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tBName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tBName.Location = new System.Drawing.Point(12, 12);
+            this.tBName.Name = "tBName";
+            this.tBName.Size = new System.Drawing.Size(424, 26);
+            this.tBName.TabIndex = 0;
+            // 
+            // tBDescription
+            // 
+            this.tBDescription.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tBDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tBDescription.Location = new System.Drawing.Point(12, 39);
+            this.tBDescription.Multiline = true;
+            this.tBDescription.Name = "tBDescription";
+            this.tBDescription.Size = new System.Drawing.Size(424, 307);
+            this.tBDescription.TabIndex = 1;
+            // 
+            // lBKeywords
+            // 
+            this.lBKeywords.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lBKeywords.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lBKeywords.FormattingEnabled = true;
+            this.lBKeywords.ItemHeight = 20;
+            this.lBKeywords.Location = new System.Drawing.Point(442, 12);
+            this.lBKeywords.Name = "lBKeywords";
+            this.lBKeywords.Size = new System.Drawing.Size(182, 364);
+            this.lBKeywords.TabIndex = 2;
+            this.lBKeywords.SelectedIndexChanged += new System.EventHandler(this.Keywords_SelectedIndexChanged);
+            // 
+            // bAdd
+            // 
+            this.bAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bAdd.Location = new System.Drawing.Point(12, 352);
+            this.bAdd.Name = "bAdd";
+            this.bAdd.Size = new System.Drawing.Size(96, 23);
+            this.bAdd.TabIndex = 3;
+            this.bAdd.Text = "Add";
+            this.bAdd.UseVisualStyleBackColor = true;
+            this.bAdd.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // bEdit
+            // 
+            this.bEdit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bEdit.Location = new System.Drawing.Point(148, 353);
+            this.bEdit.Name = "bEdit";
+            this.bEdit.Size = new System.Drawing.Size(96, 23);
+            this.bEdit.TabIndex = 4;
+            this.bEdit.Text = "Edit";
+            this.bEdit.UseVisualStyleBackColor = true;
+            this.bEdit.Click += new System.EventHandler(this.Edit_Click);
+            // 
+            // bDelete
+            // 
+            this.bDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bDelete.Location = new System.Drawing.Point(284, 352);
+            this.bDelete.Name = "bDelete";
+            this.bDelete.Size = new System.Drawing.Size(96, 23);
+            this.bDelete.TabIndex = 5;
+            this.bDelete.Text = "Delete";
+            this.bDelete.UseVisualStyleBackColor = true;
+            this.bDelete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // tBSearch
+            // 
+            this.tBSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tBSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tBSearch.Location = new System.Drawing.Point(442, 380);
+            this.tBSearch.Name = "tBSearch";
+            this.tBSearch.Size = new System.Drawing.Size(182, 26);
+            this.tBSearch.TabIndex = 6;
+            this.tBSearch.TextChanged += new System.EventHandler(this.Search_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.bAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 424);
+            this.Controls.Add(this.tBSearch);
+            this.Controls.Add(this.bDelete);
+            this.Controls.Add(this.bEdit);
+            this.Controls.Add(this.bAdd);
+            this.Controls.Add(this.lBKeywords);
+            this.Controls.Add(this.tBDescription);
+            this.Controls.Add(this.tBName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tBName;
+        private System.Windows.Forms.TextBox tBDescription;
+        private System.Windows.Forms.ListBox lBKeywords;
+        private System.Windows.Forms.Button bAdd;
+        private System.Windows.Forms.Button bEdit;
+        private System.Windows.Forms.Button bDelete;
+        private System.Windows.Forms.TextBox tBSearch;
+    }
+}
+
---
+++ MilitaryTCGUtility/Form1.cs
@@ -0,0 +1,171 @@
+﻿using MongoCRUD;
+using MongoCRUD.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MilitaryTCGUtility
+{
+
+    public partial class Form1 : Form
+    {
+        private readonly Crud<Keyword> _crudKeyword;
+        private Control _errorControl;
+        public Form1()
+        {
+            InitializeComponent();
+            //const string ConnectionString = "mongodb://ohm:741895623ohm@test-shard-00-00-imtir.mongodb.net:27017,test-shard-00-01-imtir.mongodb.net:27017,test-shard-00-02-imtir.mongodb.net:27017/test?ssl=true&replicaSet=test-shard-0&authSource=admin&retryWrites=true";
+            MongoDbConnection.InitializeAndStartConnection("MilitaryTCG", serverIP: "192.168.1.67");
+            _crudKeyword = new Crud<Keyword>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FillListBox();
+        }
+
+        public void FillListBox(IEnumerable<Keyword> keywords = null)
+        {
+            lBKeywords.Items.Clear();
+            var resKeywords = keywords ?? _crudKeyword.GetAll();
+
+            lBKeywords.Items.AddRange(resKeywords.ToArray());
+        }
+
+        private void ClearKeywordInfoTextboxes()
+        {
+            tBName.Text = tBDescription.Text = string.Empty;
+            tBName.Focus();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+
+            CheckValidation(() =>
+            {
+                var keyword = new Keyword(tBName.Text, tBDescription.Text);
+                _crudKeyword.Insert(keyword);
+                FillListBox();
+                ClearKeywordInfoTextboxes();
+            });
+
+        }
+
+        private void CheckValidation(Action action)
+        {
+            var notValidatedControl = IsValid;
+            if (notValidatedControl == null)
+            {
+
+                if (_errorControl?.BackColor == Color.Red)
+                {
+                    _errorControl.BackColor = Color.White;
+                }
+
+                action();
+            }
+            else
+            {
+                if (_errorControl != null && _errorControl != notValidatedControl)
+                {
+                    _errorControl.BackColor = Color.White;
+                }
+                _errorControl = notValidatedControl;
+                notValidatedControl.BackColor = Color.Red;
+            }
+        }
+
+        private Control IsValid
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(tBName.Text))
+                {
+                    return tBName;
+                }
+                else if (string.IsNullOrWhiteSpace(tBDescription.Text))
+                {
+                    return tBDescription;
+                }
+
+                return null;
+            }
+        }
+
+        private void Keywords_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lBKeywords.SelectedIndex != -1)
+            {
+                var keyword = (Keyword)lBKeywords.SelectedItem;
+                tBName.Text = keyword.Name;
+                tBDescription.Text = keyword.Description;
+            }
+        }
+
+        private void Edit_Click(object sender, EventArgs e)
+        {
+            CheckValidation(() =>
+            {
+                if (lBKeywords.SelectedIndex != -1)
+                {
+                    var keyword = (Keyword)lBKeywords.SelectedItem;
+                    keyword.Name = tBName.Text;
+                    keyword.Description = tBDescription.Text;
+                    _crudKeyword.Update(keyword._id, keyword);
+                    lBKeywords.SelectedIndex = -1;
+                    FillListBox();
+                    ClearKeywordInfoTextboxes();
+                }
+            });
+
+
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            if (lBKeywords.SelectedIndex != -1)
+            {
+                var keyword = (Keyword)lBKeywords.SelectedItem;
+                _crudKeyword.Delete(keyword._id);
+                lBKeywords.SelectedIndex = -1;
+                FillListBox();
+                ClearKeywordInfoTextboxes();
+            }
+        }
+
+        private void Search_TextChanged(object sender, EventArgs e)
+        {
+            if (tBSearch.TextLength>2)
+            {
+                var result = _crudKeyword.MultipleFieldSearch(tBSearch.Text, "Name", "Description");
+                FillListBox(result);
+            }
+            else
+            {
+                FillListBox();
+            }
+        }
+    }
+    public class Keyword : DbObject
+    {
+        public Keyword()
+        {
+        }
+
+        public Keyword(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
